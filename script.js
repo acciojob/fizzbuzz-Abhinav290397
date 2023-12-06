a4bc8d0996f241e5a3cbd6a552c1c2cb83b18fac
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-12-06 16:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 function fun(){
 	let res = "";
 	for(let i=1;i<=20;i++){
-		if(i%3 == 0 && i%5 == 0)res += "FizBuzz"\n;
-		else if(i%3 == 0) res += "Fizz"\n;
-	    else if(i%5 == 0)res += "Buzz"\n;
-		else res += i\n;
+		if(i%3 == 0 && i%5 == 0)res += "FizBuzz\n";
+		else if(i%3 == 0) res += "Fizz\n";
+	    else if(i%5 == 0)res += "Buzz\n";
+		else res += i + "\n";
 	}
 	alert(res);
 }
